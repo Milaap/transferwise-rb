c74622f4eadf8919f0704ede0bcde53d6bcfa980
Shireesh Anjal <shireesh@milaap.org>
2017-03-22 20:48:13 +0530

    Introduced :get method in ApiResource
    Can be used to fetch an individual record of a specific type of
    resource, given it's id e.g.
    
    /v1/transfers/#{transfer_id}
    
    Change-Id: I3fcf55327c94d34a520e18e910d17f699b7306c8

--- lib/transfer_wise/api_resource.rb
+++ lib/transfer_wise/api_resource.rb
@@ -6,7 +6,11 @@ module TransferWise
       self.name.split('::')[-1]
     end
 
-    def self.resource_url
+    def self.resource_url(resource_id)
+      "#{collection_url}/#{resource_id}"
+    end
+
+    def self.collection_url
       if self == APIResource
         raise NotImplementedError.new('APIResource is an abstract class. You should perform actions on its subclasses (Account, Transfer, etc.)')
       end
@@ -14,12 +18,17 @@ module TransferWise
     end
 
     def self.create(params={}, opts={})
-      response = TransferWise::Request.request(:post, resource_url, params, opts)
+      response = TransferWise::Request.request(:post, collection_url, params, opts)
       convert_to_transfer_wise_object(response)
     end
 
     def self.list(filters = {}, headers = {})
-      response = TransferWise::Request.request(:get, resource_url, filters, headers)
+      response = TransferWise::Request.request(:get, collection_url, filters, headers)
+      convert_to_transfer_wise_object(response)
+    end
+
+    def self.get(resource_id, headers = {})
+      response = TransferWise::Request.request(:get, resource_url(resource_id), {}, headers)
       convert_to_transfer_wise_object(response)
     end
   end
